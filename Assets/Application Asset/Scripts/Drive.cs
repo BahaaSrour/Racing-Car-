f7bc82b23ccf509f30f8969377e33d611f4b2210
BahaaSrour <dev.bahaasrour@gmail.com>
2021-04-21 07:38:45 +0200

    -initializing the wheel collider -car are added

---
+++ Assets/Application Asset/Scripts/Drive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider WC;
+    public float torque = 200;
+
+
+    void Start()
+    {
+        WC = GetComponent<WheelCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        Debug.Log("a ="+ a);
+        if(a!=0)
+        Go(a);
+    }
+
+    private void Go(float accel)
+    {
+        accel = Mathf.Clamp(accel, -1,1);
+        float thrustTorque = torque * accel;
+        WC.motorTorque = thrustTorque;
+
+        Quaternion quat;
+        Vector3 pos;
+        WC.GetWorldPose(out pos, out quat);
+        transform.position = pos;
+        transform.rotation = quat;
+
+    }
+}
