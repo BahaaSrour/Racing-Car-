8141cf9766426e08929b1bf6b5c91a3263208ce9
BahaaSrour <dev.bahaasrour@gmail.com>
2021-04-23 07:14:24 +0200

    -adding brake to the car moving system

--- Assets/Application Asset/Scripts/Drive.cs
+++ Assets/Application Asset/Scripts/Drive.cs
@@ -9,39 +9,50 @@ public class Drive : MonoBehaviour
     public float steerAngle = 30;
     public WheelCollider[] WCs;
     public GameObject[] Wheels;
-
+    public float breakTorque = 2000;
  
     // Update is called once per frame
     void Update()
     {
         float a = Input.GetAxis("Vertical");
         float steer = Input.GetAxis("Horizontal");
-        Go(a,steer);
+        float brake = Input.GetAxis("Jump");
+        Go(a,steer,brake);
         AnimiateTyres();
     }
 
-    private void Go(float accel,float steer)
+    private void Go(float accel,float steer,float brake)
     {
         accel = Mathf.Clamp(accel, -1,1);
+        brake = Mathf.Clamp(brake, 0,1)* breakTorque;
         steer = Mathf.Clamp(steer, -1,1)*steerAngle;
         float thrustTorque = torque * accel;
         for (int i = 0; i < 4; i++)
         {
             WCs[i].motorTorque = thrustTorque;
-            WCs[i].steerAngle = steer;
+            if (i < 2)
+                WCs[i].steerAngle = steer;
+            else
+                WCs[i].brakeTorque = brake;
+            Quaternion quat;
+            Vector3 pos;
+            WCs[i].GetWorldPose(out pos, out quat);
+            Wheels[i].transform.position = pos;
+            Wheels[i].transform.localRotation = quat;
         }
     }
+
     public void AnimiateTyres()
     {
-        for (int i = 0; i < 4; i++)
-        {
+        //for (int i = 0; i < 4; i++)
+        //{
 
-        Quaternion quat;
-        Vector3 pos;
-        WCs[i].GetWorldPose(out pos, out quat);
-        Wheels[i].transform.position = pos;
-            //if(i<2 )
-        Wheels[i].transform.rotation = quat;
-        }
+        ////Quaternion quat;
+        ////Vector3 pos;
+        ////WCs[i].GetWorldPose(out pos, out quat);
+        ////Wheels[i].transform.position = pos;
+        ////    if (i < 2)
+        ////        Wheels[i].transform.rotation = quat;
+        //}
     }
 }
